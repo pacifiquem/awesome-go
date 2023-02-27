c6627b0c515fccbd51fff85514f6a21b934d36f3
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-02-27 09:34:33 +0200

    [init]: initialized tailwindcss for styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Stage } from "@inlet/react-pixi";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App w-[100vw] h-[100vh] flex items-center justify-center">
+      <Stage width={1000} height={700}>
+      </Stage>
     </div>
   );
 }

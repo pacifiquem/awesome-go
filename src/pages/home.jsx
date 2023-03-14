a971da92d92d9850cc18106b68518be210a003fe
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-03-14 10:52:08 +0200

    faet: homepage added

---
+++ src/pages/home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Section1 from '../components/section1';
+import Section2 from '../components/section2';
+import Section3 from '../components/section3';
+import Section4 from '../components/section4';
+import Section5 from '../components/sectoin5';
+
+const Home  = () => {
+
+    return (
+        <div>
+            <Section1 />
+            <Section2 />
+            <Section3 />
+            <Section4 />
+            <Section4 />
+            <Section5 />
+        </div>
+    );
+
+}
+
+export default Home;

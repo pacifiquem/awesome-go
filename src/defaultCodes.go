a6de4e9cb17b4ac2ac10cbbf33c12b975cd991fa
M.Pac <pacifiquemurangwa001@gmail.com>
2023-05-04 08:53:09 +0200

    feat: added explainations for default code

---
+++ src/defaultCodes.go
@@ -0,0 +1,10 @@
+package main //package name
+
+import "fmt" // importing Go standard library for I/O
+
+func main() { //function main which is always entry point to out program and we always have one in the whole program.
+	// some codes here
+	fmt.Println("Printing !!!!!") //printing to console.
+}
+
+//you must use every decraled variable and imported library.

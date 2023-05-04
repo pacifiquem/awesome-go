f370f7872745c8d2be55b14830654ab9e4549073
M.Pac <pacifiquemurangwa001@gmail.com>
2023-05-04 16:21:41 +0200

    explained added example

--- src/pointers.go
+++ src/pointers.go
@@ -39,3 +39,18 @@ func main() {
     fmt.Printf("*p = %d\n", *p)   // *p = 40
     fmt.Printf("j = %d\n", j)     // j = 20
 }
+
+
+// In this example, we declare a variable i with an initial value of 10 using the := operator. We then declare a pointer variable p using the & operator to get the memory address of i.
+
+// We print the value of i using the variable itself (fmt.Printf("i = %d\n", i)) and the dereferenced pointer (fmt.Printf("*p = %d\n", *p)), which gives the same output of 10.
+
+// We change the value of i using the pointer by assigning a new value to the dereferenced pointer (*p = 20). This also changes the value of i, since the pointer points to the same memory address as i.
+
+// We then declare a new variable j and assign the value of i to j (j = i). This assigns the value of i to j, but does not create a new pointer to i.
+
+// We print the value of j and i using the variable itself and the dereferenced pointer, which gives the same output of 20.
+
+// We change the value of i using the variable by assigning a new value to i (i = 40). This also changes the value of the dereferenced pointer, but not the value of j.
+
+// Note that pointers in Golang are declared using the * operator before the variable name (var p *int in this case). To get the memory address of a variable, you use the & operator (p = &i in this case). To dereference
